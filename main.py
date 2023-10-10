908b50de906dd632f387a8e7bd9046a08452cc6e
SemUijen <grootveer.sem@outlook.com>
2023-10-10 20:31:11 +0200

    added accuracy functions

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ from itertools import product
 
 def main(iter_id, instance_name, solutions, solution_ids):
     print(f"started_{iter_id}")
-
+    print(len(solutions))
     INSTANCE = read(f"data/route_instances/{instance_name}.vrp", round_func="round")
 
     labels = []
     labels_cat = []
 
     # TODO: add Calculations
-    random_acc = []
-    random_acc_limit = []
+    random_acc_list = []
+    lim_random_acc_list = []
 
     couple_id = 0
     cap_pen = 400
@@ -29,8 +29,10 @@ def main(iter_id, instance_name, solutions, solution_ids):
     couple_ids = list(map(tuple, product(solution_ids, repeat=2)))
     couple_iter = product(solution_ids, repeat=2)
     print(couple_ids)
-
     for sol_idx1, sol_idx2 in couple_iter:
+
+        total_improvements = 0
+        limited_improvements = 0
         parent1, parent2 = solutions[sol_idx1 - 1], solutions[sol_idx2 - 1]
 
         numR_P1 = parent1.num_routes()
@@ -40,6 +42,7 @@ def main(iter_id, instance_name, solutions, solution_ids):
         label_shape = (numR_P1, numR_P2, Max_to_move - 1)
         label_improv = np.zeros(label_shape, dtype=float)
         label_categ = np.zeros(label_shape, dtype=int)
+
         for idx1 in tqdm(range(0, numR_P1)):
             for idx2 in range(0, numR_P2):
                 for numRoutesMove in range(1, Max_to_move):
@@ -48,13 +51,28 @@ def main(iter_id, instance_name, solutions, solution_ids):
                                                               twP=tw_pen, moves=numRoutesMove)
                     label_improv[(idx1, idx2, numRoutesMove - 1)] = abs_improv
                     label_categ[(idx1, idx2, numRoutesMove - 1)] = category
+                    if category == 4:
+                        total_improvements += 1
+                        if idx1 == idx2 or idx2 == 0:
+                            limited_improvements += 1
 
+
+        total_options = numR_P1 * numR_P2 * (Max_to_move-1)
+        limited_options = max(numR_P1, numR_P2) * (Max_to_move-1)
         labels.append(label_improv)
         labels_cat.append(label_categ)
+        random_acc_list.append(total_improvements/total_options)
+        lim_random_acc_list.append(limited_improvements/limited_options)
 
     raw_data = {
-        "route_instance_name": instance_name}
-
+        "route_instance_name": instance_name,
+        "parent_routes": solutions,
+        "parent_couple_idx": couple_ids,
+        "labels": labels,
+        "labels_cat": labels_cat,
+        "random_acc": random_acc_list,
+        "random_acc_limit": lim_random_acc_list,
+    }
     with open(f"data/raw_model_data/{instance_name}_rawdata_{iter_id}.pkl", "wb") as handle:
         pickle.dump(raw_data, handle)
 
@@ -63,8 +81,10 @@ def main(iter_id, instance_name, solutions, solution_ids):
 
 if __name__ == "__main__":
 
-    instance_name = 'X-n439-k37'
-    with open(f"data/solutions/just_solutions.pkl", 'rb') as handle:
+
+    with open(f"data/raw_model_data/X-n439-k37_rawdata_0.pkl", 'rb') as handle:
         solutions = pickle.load(handle)
 
-    main(0, instance_name, solutions[0:2], [1, 2])
+
+    print(solutions)
+

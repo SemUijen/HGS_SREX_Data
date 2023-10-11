5675f8e1760d90b17b0dfcb95746175619986941
SemUijen <grootveer.sem@outlook.com>
2023-10-11 09:27:15 +0200

    added permutation todo

--- main.py
+++ main.py
@@ -67,4 +67,6 @@ if __name__ == "__main__":
     with open(f"data/solutions/just_solutions.pkl", 'rb') as handle:
         solutions = pickle.load(handle)
 
+
+    # TODO: Use permutations instead of product from itertools it has no combinations with itself i.e. (1,1) (2,2)
     main(0, instance_name, solutions[0:2], [1, 2])

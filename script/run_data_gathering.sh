2bdf1f798e87db2e8d37f8af47bd2364f686011b
SemUijen <grootveer.sem@outlook.com>
2023-10-13 14:39:59 +0200

    reverted

--- script/run_data_gathering.sh
+++ script/run_data_gathering.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=test_creation_speed
-#SBATCH --output=logging/test_creation_speed-%j.out
-#SBATCH --error=logging/test_creation_speed-%j.error
+#SBATCH --output=test_creation_speed-%j.out
+#SBATCH --error=test_creation_speed-%j.error
 #SBATCH --chdir /home/suijen/HGS_SREX_Data
 #SBATCH --export=ALL
 #SBATCH --get-user-env=L
@@ -15,10 +15,16 @@
 #SBATCH --mail-user=s.j.uijen@tilburguniversity.edu
 
 # Setup modules
-module load 2022
-module load Python/3.10.4-GCCcore-11.3.0
-module purge
-
+if [ ! -z "${SLURM_JOB_ID}" ]; then
+    module purge
+    module load 2022
+    module load aiohttp/3.8.3-GCCcore-11.3.0
+    module load matplotlib/3.5.2-foss-2022a
+    module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+    module load SciPy-bundle/2022.05-foss-2022a
+    module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+    module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
+fi
 
 # Log versions
 echo "PYTHON_VERSION = $(python3.10 --version)"
@@ -35,6 +41,7 @@ pip3.10 install -q \
     tqdm \
     numpy \
     pyvrp \
+
 
 
 # Run experiment
